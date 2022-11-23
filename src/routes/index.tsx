7f2119c1a885f62bdc89d1d2038540271a50e0ed
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-11-22 23:15:20 -0300

    chore: add home n cart

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
