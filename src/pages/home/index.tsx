799ae7bcb0d739a4a6f90994f7d8befd4e13b15f
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-11-23 22:29:39 -0300

    data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { View, Text } from 'react-native';
   
 export default function Home() {
+
+    const data = [{
+        
+    }]
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
