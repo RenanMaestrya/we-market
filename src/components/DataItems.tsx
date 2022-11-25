7343a20369e2c8a29d72e47144835a9c14f66e24
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-11-25 18:01:11 -0300

    chore: modify icons and componentization data

---
+++ src/components/DataItems.tsx
@@ -0,0 +1,22 @@
+export const DATA = [
+    {
+        id: '1',
+        name: "Macarrão Instantâneo",
+        price: 1.95,
+    },
+    {
+        id: '2',
+        name: "Sabonete",
+        price: 3,
+    },
+    {
+        id: '3',
+        name: "Picanha",
+        price: 61.90,
+    },
+    {
+        id: '4',
+        name: "Cerveja",
+        price: 2.95,
+    },
+  ]
